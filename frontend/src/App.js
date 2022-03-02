6201e325b917424c99e67662405bcb7f41aa46bf
saineha2000 <81003565+saineha2000@users.noreply.github.com>
2022-03-02 16:41:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
 
 import SearchableDropdown from "./Components/SearchableDropDown/SearchableDropDown";
+import { useState } from "react";
+import { Grid } from "@mui/material";
 function App() {
+  const country_url = "http://localhost:8080/api/v1/country";
+  const [value, setValue] = useState(null);
+  const [sname, setSname] = useState(null);
   return (
     <div className="App">
-      SearchableDropdown/>
+      <Grid sx={{ margin: 5 }}>
+        <SearchableDropDown
+          prop_name={"name"} //The property which should appear in dropdown
+          url={country_url} //The rest Url
+          prop_id={"id"} //the property to compare fields .Most probably it would be the primary key of database
+          label="select a country" //The label u want to add
+          onChange={(val) => {
+            setValue(val);
+          }}
+          isMulti
+        />
+        <br />
+        <SearchableDropDown
+          prop_name={"sortname"}
+          url={country_url}
+          prop_id={"id"}
+          label="short names of countries"
+          onChange={(val) => {
+            setSname(val);
+          }}
+        />
+        <br></br>
+        dropdown1:{value !== null && value.name}
+        <br></br>
+        dropdown2:{sname !== null && sname.sortname}
+      </Grid>
     </div>
   );
 }
